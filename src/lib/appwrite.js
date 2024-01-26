b24097535156d870c0ef3e8018233d2399235bee
prathamesh <prathameshsk94@gmail.com>
2024-01-26 17:16:32 +0530

    .

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,9 +1,7 @@
 import { Client, Databases, Account } from "appwrite";
-
+import conf from "../conf";
 const client = new Client();
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("65afc27345c30629fe76"); // Replace with your project ID
+client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
